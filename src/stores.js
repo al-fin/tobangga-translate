2a77b6875256389edc166fdb83b2ffc2b04f3e79
Alfin <m.alfin.syahruddin@gmail.com>
2020-07-21 15:21:37 +0700

    Fix bug

--- src/stores.js
+++ src/stores.js
@@ -1,7 +1,7 @@
 import { writable, derived, get } from "svelte/store";
 import { translate } from "./core.js";
 
-export const lang = writable(localStorage.getItem("lang") || "");
+export const lang = writable(localStorage.getItem("lang") || "to");
 export const input = writable("");
 export const output = derived([lang, input], ([$lang, $input], set) => {
   console.log("lang", get(lang));
